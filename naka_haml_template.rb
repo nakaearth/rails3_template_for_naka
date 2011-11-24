c6731b04d7f46abd82301998b2bb7ce5803e435f
Nakamura shinichirou <naka5313@gmail.com>
2011-11-25 00:28:05 +0900

    update

---
+++ naka_haml_template.rb
@@ -0,0 +1,27 @@
+puts "naka template 3_0"
+run 'rm public/index.html'
+run 'rm public/images/rails.png'
+
+puts "Gemfile edit!!"
+gem 'omniauth','0.3.2'
+gem 'haml-rails'
+gem 'json'
+gem 'will_paginate'
+gem 'jquery-rails', '>= 0.2.6'
+gem 'jpmobile',  :require => 'action_pack'
+gem 'bartt-ssl_requirement', :require => 'ssl_requirement'
+gem 'sprockets', '~> 2.0'
+gem 'dalli'
+gem 'whenever'
+gem "carrierwave"
+gem 'rspec'
+gem 'rspec-rails',:group=>:development
+
+puts "git init!!"
+git :init
+git :add=>'.'
+git :commit=>"-m'initial commit'"
+
+puts "done"
+
+
